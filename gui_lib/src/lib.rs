189e02f0bd4e3259eaebb6eb3178f0fb6b830d94
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-05-07 17:45:05 +0530

    trait objs

---
+++ gui_lib/src/lib.rs
@@ -0,0 +1,27 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for comp in self.components.iter() {
+            comp.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // draw button
+    }
+}
